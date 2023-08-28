c04d27d3e96e82ab4dde0200d6b4a60d69065116
alex23job <alex23job@yandex.ru>
2023-08-29 02:22:13 +0300

    final

--- js/script.js
+++ js/script.js
@@ -147,9 +147,40 @@ const observer = new IntersectionObserver(
     },
 );
 
+const openFilter = (btn, dropDown, classNameBtn, classNameDd) => {
+    dropDown.style.height = `${dropDown.scrollHeight}px`;
+    btn.classList.add(classNameBtn);
+    dropDown.classList.add(classNameDd);
+}
+
+const closeFilter = (btn, dropDown, classNameBtn, classNameDd) => {
+    dropDown.style.height = "";
+    btn.classList.remove(classNameBtn);
+    dropDown.classList.remove(classNameDd);
+}
+
 const init = () => {
     const filterForm = document.querySelector('.filter__form');
     const cardsList = document.querySelector('.cards__list');
+    const vacanciesFilterBtn = document.querySelector('.vacancies__filter-btn');
+    const vacanciesFilter = document.querySelector('.vacancies__filter');
+
+    vacanciesFilterBtn.addEventListener('click', () => {
+        if (vacanciesFilterBtn.classList.contains('vacancies__filter-btn_active')) {
+            closeFilter(vacanciesFilterBtn, vacanciesFilter, 'vacancies__filter-btn_active', 'vacancies__filter_active',);
+        } else {
+            openFilter(vacanciesFilterBtn, vacanciesFilter, 'vacancies__filter-btn_active', 'vacancies__filter_active',);
+        }
+    });
+
+    window.addEventListener('resize', () => {
+        if (vacanciesFilterBtn.classList.contains('vacancies__filter-btn_active')) {
+            // 1) 
+            // vacanciesFilter.style.height = `${vacanciesFilter.scrollHeight}px`;
+            // 2)
+            closeFilter(vacanciesFilterBtn, vacanciesFilter, 'vacancies__filter-btn_active', 'vacancies__filter_active',);
+        }
+    });
     
     // select city
     const citySelect = document.querySelector("#city");
@@ -190,6 +221,16 @@ const init = () => {
         }
     });
 
+    cardsList.addEventListener('keydown', ({ code, target }) => {
+        const vacancyCard = target.closest('.vacancy');
+        if ((code === 'Enter' || code === 'NumpadEnter') && vacancyCard) {
+            const vacancyId = vacancyCard.dataset.id;
+            //console.log("vacancyId : ", vacancyId);
+            openModal(vacancyId);
+            target.blur();
+        }
+    });
+
     // filter
     filterForm.addEventListener('submit', (event) => {
         event.preventDefault();
@@ -202,9 +243,11 @@ const init = () => {
 
         urlWithParam.searchParams.set("limit", window.innerWidth < 768 ? 6 : 12);
         urlWithParam.searchParams.set("page", 1);
-        
+
         getData(urlWithParam, renderVacancy, renderError).then(() => {
             lastUrl = urlWithParam;
+        }).then(() => {
+            closeFilter(vacanciesFilterBtn, vacanciesFilter, 'vacancies__filter-btn_active', 'vacancies__filter_active',);
         });
     });
 };
